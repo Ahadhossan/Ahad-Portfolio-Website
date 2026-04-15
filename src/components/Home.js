2f74aee4def2bc32772051631ae3cb51b0380fef
Md Ahad Hossain <117075956+Ahadhossan@users.noreply.github.com>
2026-04-15 23:14:37 +0600

    Delete src directory

--- src/components/Home.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Home = () => {
-    return (_jsx("div", { className: "text-black p-8", children: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem aliquid nam minima sunt mollitia fuga ipsa, porro, reiciendis magni voluptatum accusantium debitis ex. Adipisci quo consequuntur repudiandae mollitia modi veritatis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequuntur placeat ipsam earum quod commodi. Eos, ullam praesentium nostrum nobis officia cum, labore maiores voluptates nesciunt rem id minus esse?" }));
-};
-export default Home;
