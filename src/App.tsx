fc649d2a56fd8fc6301a917a5ad8a7c314e1141c
Md Ahad Hossain <ahadm3016@gmail.com>
2026-04-15 16:54:05 +0600

    feat: add Home component and implement StaggeredMenu in Navbar
    - Introduced a new Home component with placeholder content.
    - Updated Navbar to include a StaggeredMenu with menu and social items.
    - Integrated GSAP for animations in StaggeredMenu.
    - Enhanced routing in App component to include Home.
    - Updated package.json with new dependencies for styling and animations.
    - Refactored index.css to include new styles and themes.
    - Added utility functions for class name management.
    - Configured TypeScript for improved type checking and module resolution.
    - Updated Vite configuration to support path aliases.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
+    <div className="overflow-hidden">
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-black p-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem aliquid
+      nam minima sunt mollitia fuga ipsa, porro, reiciendis magni voluptatum
+      accusantium debitis ex. Adipisci quo consequuntur repudiandae mollitia
+      modi veritatis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Doloribus consequuntur placeat ipsam earum quod commodi. Eos, ullam
+      praesentium nostrum nobis officia cum, labore maiores voluptates nesciunt
+      rem id minus esse?
+    </div>
+  );
+};
+
+export default Home;
