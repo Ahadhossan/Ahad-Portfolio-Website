9de680fb2f522e08e297e7f15fe382b5d253141d
Md Ahad Hossain <ahadm3016@gmail.com>
2026-04-24 01:08:45 +0600

    Update About component with detailed description

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const About: React.FC = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      About. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa eum
+      expedita porro. Minima eos quas omnis ipsam sed sunt nobis expedita
+      quibusdam nemo voluptates, debitis iusto asperiores non, quidem aliquid.
+    </div>
+  );
 };
 
 export default About;
