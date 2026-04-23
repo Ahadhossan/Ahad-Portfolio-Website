b4db040c6e6fbc9c5173c62f05e8cc7ea29b8f6f
Md Ahad Hossain <ahadm3016@gmail.com>
2026-04-24 01:32:14 +0600

    Refactor About component to simplify content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const About: React.FC = () => {
-  return (
-    <div>
-      About. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa eum
-      expedita porro. Minima eos quas omnis ipsam sed sunt nobis expedita
-      quibusdam nemo voluptates, debitis iusto asperiores non, quidem aliquid.
-    </div>
-  );
+  return <div>About.</div>;
 };
 
 export default About;
